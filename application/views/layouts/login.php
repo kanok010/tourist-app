3a4db83c4e5f389f4214c57524759d8a4bfa2707
Kanok76 <Kanok76@SSV_T13-Kanok76.true.th>
2016-09-16 15:40:43 +0700

    edit text and change style

--- application/views/layouts/login.php
+++ application/views/layouts/login.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Log in</title>
+  <title>Tourist Administrator | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
@@ -27,7 +27,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+      <i class="text-red fa fa-map-marker" aria-hidden="true"></i> <b>Tourist</b> App
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
@@ -37,7 +37,7 @@
    <?php echo form_open('verifylogin'); ?>
       <div class="form-group has-feedback">
         <input type="text" name="username" class="form-control" placeholder="Username">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
         <input type="password" name="password" class="form-control" placeholder="Password">
@@ -53,7 +53,7 @@
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <button type="submit" class="btn btn-danger btn-block btn-flat">Sign In</button>
         </div>
         <!-- /.col -->
       </div>
