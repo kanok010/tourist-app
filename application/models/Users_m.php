9e5ecf938d2fc4efa1e843d47a4fae1e76ecc76c
Kanok76 <Kanok76@SSV_T13-Kanok76.true.th>
2016-09-20 12:30:37 +0700

    add function

--- application/models/Users_m.php
+++ application/models/Users_m.php
@@ -11,13 +11,42 @@
  *
  * @author Kanok76
  */
-class Users_m extends CI_Model{
+class Users_m extends CI_Model{    
     function get_data() {
         $this->db->select('*');
-        $this->db->from('member');
+        $this->db->from('users');
         $query = $this->db->get();
         $data =  $query->result_array();
-        print_r($data);
+        return $data;
+    }
+    
+    public function get_users($id) {
+        if($id != FALSE) {
+          $query = $this->db->get_where('users', array('id' => $id));
+          return $query->row_array();
+        }
+        else {
+          return FALSE;
+        }
+    }
+    
+    function get_item_by_id($id) {
+        $this->db->select('*');
+        $this->db->from('users');
+        $this->db->where('id', $id);
+        $this->db->limit(1);
+
+        $query = $this->db->get();
+
+        if($query -> num_rows() == 1)
+        {
+            return $query->result();
+        }
+        else
+        {
+            return false;
+        }
+      
     }
     
     function login($username, $password)
