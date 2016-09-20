8a0b71dafb6f078f04eafd1086be5a16db6a5674
Kanok76 <Kanok76@SSV_T13-Kanok76.true.th>
2016-09-20 12:31:56 +0700

    add user controller

---
+++ application/controllers/Users.php
@@ -0,0 +1,41 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users extends CI_Controller {
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->load->database();
+        $this->load->model('Users_m');
+    }
+    
+    public function index() {
+
+        
+        if($this->session->userdata('logged_in'))
+        {
+            
+            $session_data = $this->session->userdata('logged_in');
+            $users = $this->Users_m->get_data();
+
+            $data = array(
+                'users'=>$users,
+                'username'=>$session_data['username'],
+                'content'=>'users/index'
+            );
+            $this->load->view('layouts/default',$data);
+        }else{
+            //If no session, redirect to login page
+            redirect('login', 'refresh');
+        }
+
+    }
+
+	
+
+	
+	
+}
