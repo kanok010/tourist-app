db487ed4075b61b860f095390ebb60f1e1e69a54
Kanok76 <Kanok76@SSV_T13-Kanok76.true.th>
2016-09-30 16:27:11 +0700

    add function create

--- application/controllers/Reward.php
+++ application/controllers/Reward.php
@@ -37,4 +37,51 @@ class Reward extends CI_Controller{
         }
 
     }
+    
+    function create() {
+        if($this->session->userdata('logged_in'))
+        {
+            $session_data = $this->session->userdata('logged_in');
+            //Including validation library
+            $this->load->library('form_validation');
+
+            $this->form_validation->set_error_delimiters('<div class="alert alert-warning alert-dismissible">', '</div>');
+
+            //Validating Package Name Field
+            $this->form_validation->set_rules('package_name', 'Package Name', 'required');
+
+            //Validating Price Plan Field
+            $this->form_validation->set_rules('price_plan', 'Price Plan', 'required');
+
+            //Validating Status Field
+            $this->form_validation->set_rules('status', 'Status', 'required');
+
+            if ($this->form_validation->run() == FALSE) {
+                $this->_render((object)array(
+                           'data' => '',
+                           'username'=>$session_data['username'],
+                           'content'=>"reward/create",
+                           'menu'=>'price-plan-add',
+                           'js_files' => array() ,
+                           'css_files' => array()));
+            } else {
+                //Setting values for tabel columns
+                $data = array
+                (                 
+                  'package_name' => $this->input->post('package_name'),
+                  'price_plan' => $this->input->post('price_plan'),
+                  'status' => $this->input->post('status')
+                );
+                //Transfering data to Model
+                if($this->Reward_m->create($data)){
+                     $this->session->set_flashdata('message','Data Inserted Successfully');                     
+                      redirect('reward');               
+                }              
+            }
+            
+        }else{
+            //If no session, redirect to login page
+            redirect('login', 'refresh');
+        }
+    }
 }
