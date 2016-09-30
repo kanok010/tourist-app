16d447848734065b450217b60dad9e1ea32718e7
Kanok76 <Kanok76@SSV_T13-Kanok76.true.th>
2016-09-30 10:07:46 +0700

    remove function login

--- application/models/Users_m.php
+++ application/models/Users_m.php
@@ -1,16 +1,4 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Users_m
- *
- * @author Kanok76
- */
 class Users_m extends CI_Model{    
     function get_data() {
         $this->db->select('*');
@@ -49,24 +37,6 @@ class Users_m extends CI_Model{
       
     }
     
-    function login($username, $password)
-     {
-       $this->db->select('id, username, password');
-       $this->db->from('users');
-       $this->db->where('username', $username);
-       $this->db->where('password', MD5($password));
-       $this->db->limit(1);
 
-       $query = $this->db->get();
-
-       if($query -> num_rows() == 1)
-       {
-         return $query->result();
-       }
-       else
-       {
-         return false;
-       }
-     }
     
 }
